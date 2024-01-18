224e274d287a489e6f5659b7c311f65abb316653
clive <clivekakeeto48@gmail.com>
2024-01-18 06:09:36 +0300

    message-card-detarched

---
+++ resources/views/livewire/chat-box/message-card.blade.php
@@ -0,0 +1,20 @@
+<div class="flex flex-col space-y-4 p-4">
+    @foreach ($messages as $message)
+        <div class="flex rounded-lg p-4 @if (isset($message['role']) && $message['role'] === 'assistant') bg-green-200 flex-reverse @else bg-blue-200 @endif ">
+            <div class="ml-4">
+                <div class="text-lg">
+                    @if (isset($message['role']) && $message['role'] === 'assistant')
+                        <a href="#" class="font-medium text-gray-900">Your Assistant</a>
+                    @else
+                        <a href="#" class="font-medium text-gray-900">You</a>
+                    @endif
+                </div>
+                <div class="mt-1">
+                    @if (isset($message['content']))
+                        <p class="text-gray-600">{!! \Illuminate\Mail\Markdown::parse($message['content']) !!}</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
